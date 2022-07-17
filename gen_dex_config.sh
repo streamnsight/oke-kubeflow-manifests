a7b89181bc9eda437b2a62006d917f795abcaaf7
Emmanuel Leroy <emmanuel.leroy@oracle.com>
2022-07-17 16:34:22 -0700

    Refactor to use Kustomize Components.

--- gen_dex_config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read -p "IDCS URL: " IDCS_URL
-read -p "IDCS Client ID: " IDCS_CLIENT_ID
-read -p "IDCS Client Secret: " IDCS_CLIENT_SECRET
-
-eval "echo \"$(cat ./deployments/add-ons/dex-idcs/config.yaml.tmpl)\"" > ./deployments/add-ons/dex-idcs/idcs-config.yaml
--- set_admin_pwd.sh
+++ set_admin_pwd.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-pushd upstream/common/dex/base/ || exit 1
-
 pip3 install bcrypt passlib -q
 
-read -p "Admin Email: " EMAIL
+if [[ -z "${ADMIN_EMAIL}" ]]; then
+    read -p "Admin Email: " ADMIN_EMAIL
+fi
 PASSWD_HASH=$(python3 -c 'from passlib.hash import bcrypt; import getpass; print(bcrypt.using(rounds=12, ident="2y").hash(getpass.getpass()))')
-cp config-map.yaml config-map.yaml.DEFAULT
-USERNAME=${EMAIL%@*}
-sed -i "" -e "s|hash:.*|hash: $PASSWD_HASH|" -e "s|email:.*|email: $EMAIL|" -e "s|username:.*|username: ${USERNAME}|" config-map.yaml
-popd || exit 1
-echo "user=${EMAIL}" > upstream/common/user-namespace/base/params.env
-echo "profile-name=kubeflow-${EMAIL//[.@_]/-}" >> upstream/common/user-namespace/base/params.env
+USERNAME=${ADMIN_EMAIL%@*}
+sed -i "" -e "s|hash:.*|hash: $PASSWD_HASH|" -e "s|email:.*|email: $ADMIN_EMAIL|" -e "s|username:.*|username: ${USERNAME}|" ./upstream/common/dex/base/config-map.yaml
+
+echo "user=${ADMIN_EMAIL}" > upstream/common/user-namespace/base/params.env
+echo "profile-name=kubeflow-${ADMIN_EMAIL//[.@_]/-}" >> upstream/common/user-namespace/base/params.env
 
