3d52bb996d497e12b2f13d6dd7d0024824e16e57
Emmanuel Leroy <emmanuel.leroy@oracle.com>
2022-09-07 15:04:22 -0700

    switch version to v1.6.0

--- get_upstream.sh
+++ get_upstream.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -rf upstream
-export KUBEFLOW_RELEASE_VERSION="v1.6.0-rc.3"
+export KUBEFLOW_RELEASE_VERSION="v1.6.0"
 git clone --branch ${KUBEFLOW_RELEASE_VERSION} https://github.com/kubeflow/manifests.git --single-branch upstream
