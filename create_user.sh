3817e3ab11ae66cdfa60a1d231f47b1752346e85
Emmanuel Leroy <emmanuel.leroy@oracle.com>
2022-10-01 18:37:55 -0700

    dns01

---
+++ create_user.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+read -p "User Email: " EMAIL
+
+if [[ -z $EMAIL ]]; then
+    echo "Please provide an email."
+    exit 1
+fi
+
+export DEFAULT_USERNAME=$(echo $EMAIL | tr '._@' '---')
+
+read -p "Username/Namespace (defaults to '${DEFAULT_USERNAME}'): " USERNAME
+
+if [[ -z ${USERNAME} ]]; then
+    export USERNAME=$DEFAULT_USERNAME
+fi
+
+eval "echo \"$(cat ./oci/profile/user-profile.yaml.tmpl)\"" > ./oci/profile/${USERNAME}.Profile.yaml
+
+echo "Profile manifest created at './oci/profile/${USERNAME}.Profile.yaml'"
+
+kubectl apply -f oci/profile/${USERNAME}.Profile.yaml
